bf91bfc92d6c127b07cff3463a0c9a3b7f723439
xuangu-fang <xuangufang@gmail.com>
2024-02-06 07:31:18 -0700

    get group-wise analysis results

--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,16 @@
 
 import pandas as  pd
 import os
+import numpy as np
+import utils
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score
+from sklearn.model_selection import KFold
 
 def missing_value_analysis(data):
 
@@ -91,9 +101,170 @@ def feature_filter(data, missing_ratio_thr,column_to_variable_dict):
             f.write("%s (%s)\n" % (item, column_to_variable_dict[item]))
 
     return used_features, not_used_features, folder_name
+
+
+def feature_type_analysis(data_new, used_features, non_feature_list):
+    """
+    extract the numerical and categorical features from the used features
+    """
+
+# go through all used features, check the num of the categories of each feature: if the num of categories > 10, then this feature is a continuous/numerical feature, otherwise, this feature is a categorical feature-> need to do one-hot encoding
+
+    numerical_feature_list = []
+    categorical_feature_list = []
+
+    for feature in used_features:
+
+        if feature not in non_feature_list:
+
+            if len(data_new[feature].value_counts()) > 10:
+                numerical_feature_list.append(feature)
+            else:
+                categorical_feature_list.append(feature)
+
+    print('number of numerical features: ', len(numerical_feature_list))
+
+    print('number of categorical features: ', len(categorical_feature_list))
+
+    print('numerical features list:',numerical_feature_list)
+
+    return numerical_feature_list, categorical_feature_list
+
+
+def group_split_religon(data_new):
+    '''group split by religon'''
+
+    # religion  {0.0: '0. DK; NA; refused to answer; no Pre IW; no Post IW;', 1.0: '1. Protestant', 2.0: '2. Catholic [Roman Catholic]', 3.0: '3. Jewish', 4.0: '4. Other and none (also includes DK preference)'}
+
+    # print(data_new['religion'].value_counts())
+
+    data_religion_dict={}
+
+    data_religion_dict['Protestant'] = data_new[data_new['religion'] == 1]
+    data_religion_dict['Catholic']  = data_new[data_new['religion'] == 2]
+    data_religion_dict['Jewish']      = data_new[data_new['religion'] == 3]
+    data_religion_dict['Other']  = data_new[(data_new['religion'] == 4 ) | (data_new['religion'] == 0)]
+
+    print('number of samples of Protestant: ', len(data_religion_dict['Protestant']))
+    print('number of samples of Catholic: ', len(data_religion_dict['Catholic']))
+    print('number of samples of Jewish: ', len(data_religion_dict['Jewish'] ))
+    print('number of samples of Other: ', len(data_religion_dict['Other'] ))
+        
+
+    return data_religion_dict
+
+def group_split_race7(data_new):
+
+    '''group split by race
+
+    Race7  
+    {1.0: '1. White non-Hispanic (1948-2012)', 
+    2.0: '2. Black non-Hispanic (1948-2012)', 
+    3.0: '3. Asian or Pacific Islander, non-Hispanic (1966-2012)', 4.0: 
+    '4. American Indian or Alaska Native non-Hispanic (1966-2012)',
+    5.0: '5. Hispanic (1966-2012)', 
+    6.0: '6. Other or multiple races, non-Hispanic (1968-2012)',
+    7.0: '7. Non-white and non-black (1948-1964)', 9.0: '9. Missing'}
+
+    '''
+    # print(data_new['Race3'].value_counts())
+
+    # print(data_new['Race7'].value_counts())
+
+    data_race7_dict={}
+    data_race7_dict['White'] = data_new[data_new['Race7'] == 1]
+    data_race7_dict['Black'] = data_new[data_new['Race7'] == 2]
+    data_race7_dict['Asian'] = data_new[data_new['Race7'] == 3]
+    data_race7_dict['American_Indian'] = data_new[data_new['Race7'] == 4]
+    data_race7_dict['Hispanic'] = data_new[data_new['Race7'] == 5]
+    data_race7_dict['Other'] = data_new[(data_new['Race7'] == 6) | (data_new['Race7'] == 7) | (data_new['Race7'] == 9)]
+
+    print('number of samples of White: ', len(data_race7_dict['White']))
+    print('number of samples of Black: ', len(data_race7_dict['Black']))
+    print('number of samples of Asian: ', len(data_race7_dict['Asian']))
+    print('number of samples of American_Indian: ', len(data_race7_dict['American_Indian']))
+    print('number of samples of Hispanic: ', len(data_race7_dict['Hispanic']))
+    print('number of samples of Other: ', len(data_race7_dict['Other']))
+
+    return data_race7_dict
+
+
+
+def get_feature_name_category_name(string, enc, value_label_dict):
+    feature_id = int(string.split('_')[0][1:])
+    category_index = int(float(string.split('_')[1]))
+
+    feature_name = enc.feature_names_in_[feature_id]
+
+    if category_index == -1:
+        category_name = 'Missing'
+    else:
+        category_name = value_label_dict[feature_name][category_index]
+
+    return feature_name, category_name
+
+def enc_feature_list(initial_list, enc, value_label_dict):
+    new_list = []
+
+    for string in initial_list:
+        feature_name, category_name = get_feature_name_category_name(string, enc, value_label_dict)
+        new_list.append((feature_name+'_'+ category_name))
+    return new_list
+
+
+def feature_process(data, numerical_feature_list, categorical_feature_list, target_variable,value_label_dict):
+    data_XY = data[numerical_feature_list + categorical_feature_list+[target_variable]]
+    # data_XY = data_XY[data_XY.notnull().all(axis=1)]
+    data_XY = data_XY.reset_index(drop=True)
+    print(data_XY.shape)
+
+    X_continuous = data_XY[numerical_feature_list]
+    X_categorical = data_XY[categorical_feature_list]
+    Y_target = data_XY[target_variable]
+
+    # impute + process(one-hot)  categorical features (also get the new names)
+
+    X_categorical_imp = X_categorical.fillna(-1)
+
+    enc = OneHotEncoder(handle_unknown='ignore')
+
+    enc.fit(X_categorical_imp)
+
+    X_categorical_transformed = enc.transform(X_categorical_imp).toarray()
+
+    initial_list = enc.get_feature_names().tolist()
+    enc_categorical_feature_list = utils.enc_feature_list(initial_list, enc, value_label_dict)    
+
+    #impute + process(normalize) the numerical features
+    imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+    X_continuous_imp = imp.fit_transform(X_continuous)
+
+    X_continuous_transformed = StandardScaler().fit_transform(X_continuous_imp)
+
+
+    return X_categorical_transformed, X_continuous_transformed, Y_target, enc_categorical_feature_list
+
+
+
+def cross_validation(X, Y, model, k = 5):
     
-
-
-
-
-
+    kf = KFold(n_splits=k, random_state=1, shuffle=True)
+    accuracy_list = []
+    recall_list = []
+    precision_list = []
+    f1_list = []
+    roc_auc_list = []
+    importance_list = []
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = Y[train_index], Y[test_index]
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        accuracy_list.append(accuracy_score(y_test, y_pred))
+        recall_list.append(recall_score(y_test, y_pred))
+        precision_list.append(precision_score(y_test, y_pred))
+        f1_list.append(f1_score(y_test, y_pred))
+        roc_auc_list.append(roc_auc_score(y_test, y_pred))
+        importance_list.append(model.coef_[0])
+    return accuracy_list, recall_list, precision_list, f1_list, roc_auc_list, importance_list
